c92f4da750f4f967d4c096b79e44d439f18f4a2e
Jennifer Miller <jmiller@archersystems.us>
2019-12-31 00:28:48 +0000

    Automatically backed up by Learn

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -19,3 +19,16 @@ def initialize(student_hash)
      end
 
   end
+  
+  def add_student_attributes(attributes_hash)
+    attributes_hash.each do |attr, value|
+     self.send("#{attr}=", value)
+
+    end
+    self
+  end
+
+  def self.all
+    @@all
+  end
+end 
