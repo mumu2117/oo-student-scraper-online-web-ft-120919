3877ab46a1aad42d90e7c49f57ff580a17210d2e
Jennifer Miller <jmiller@archersystems.us>
2019-12-31 00:27:14 +0000

    Automatically backed up by Learn

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,3 +1,13 @@
 class Student
    attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
   @@all = []
+
+def initialize(student_hash)
+
+    student_hash.each do |attribute, value|
+  end	      self.send("#{attribute}=", value)
+
+    end
+  def self.create_from_collection(students_array)	    @@all << self
+
+  end
